15d7b60e16bc2c99689e248c91d810af03f8ae36
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-09-29 15:44:28 +0900

    add sidebar

--- resources/js/Pages/Top.vue
+++ resources/js/Pages/Top.vue
@@ -1,9 +1,18 @@
 <template>
+<div>
+  <sidebar />
   <b-container>Top page</b-container>
+</div>
 </template>
 
 <script>
+import Sidebar from '../components/Sidebar.vue'
+
 export default {
   name: 'Top',
+
+  components: {
+    Sidebar,
+  }
 }
 </script>
